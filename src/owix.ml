b5bfea3e643d5e3bbab2e4b31e52b0148b37b543
Raja Boujbel <rjbou@ocamlpro.com>
2023-06-27 13:51:16 +0200

    opam pluginfy owix

--- src/owix.ml
+++ src/owix.ml
@@ -1,7 +1,11 @@
 open Cmdliner
+open OpamTypes
+open OpamStateTypes
 
 type config = {
-  path : string;
+  package : OpamPackage.Name.t;
+  path : string option;
+  binary: string option;
   output_dir : string;
   wix_path : string;
   package_guid: string option;
@@ -13,8 +17,16 @@ type config = {
 module Args = struct
   open Arg
 
+  let package =
+    required & pos 0 (some OpamArg.package_name) None & info [] ~docv:"PACKAGE"
+    ~doc:"The package to create an installer"
+
   let path =
-    required & pos 0 (some file) None & info [] ~docv:"PROGRAM" ~doc:
+    value & opt (some file) None & info ["binary-path";"bp"] ~docv:"PROGRAM" ~doc:
+    "The path to the binary file to handle"
+
+  let binary =
+    value & opt (some string) None & info ["binary";"b"] ~docv:"PROGRAM" ~doc:
     "The path to the binary file to handle"
 
   let output_dir =
@@ -42,10 +54,10 @@ module Args = struct
     "BMP file that is used as background for banner."
 
   let term =
-    let apply path output_dir wix_path package_guid icon_file dlg_bmp ban_bmp =
-      { path; output_dir; wix_path; package_guid; icon_file; dlg_bmp; ban_bmp }
+    let apply package path binary output_dir wix_path package_guid icon_file dlg_bmp ban_bmp =
+      { package; path; binary; output_dir; wix_path; package_guid; icon_file; dlg_bmp; ban_bmp }
     in
-    Term.(const apply $ path $ output_dir $ wix_path $ package_guid $ icon_file $
+    Term.(const apply $ package $ path $ binary $ output_dir $ wix_path $ package_guid $ icon_file $
       dlg_bmp $ ban_bmp)
 
 end
@@ -55,35 +67,103 @@ let write_to_file path content =
   Out_channel.output_string oc content;
   close_out oc
 
-let create_bundle conf =
-  let dlls =Cygcheck.get_dlls conf.path in
-  let bundle_dir = Filename.concat conf.output_dir
-    (Filename.basename conf.path) in
-  if Sys.file_exists bundle_dir then begin
-    System.call_unit System.Remove (true, bundle_dir)
-  end;
-  System.call_unit System.Mkdir (true, bundle_dir);
-  List.iter (fun dll ->
-    System.call_unit System.Copy (dll, bundle_dir)
-    ) dlls;
-  let exe_file =
-    let base = Filename.basename conf.path in
-    if not (Filename.extension base = "exe")
-    then base ^ ".exe"
-    else base
-  in
-    System.call_unit System.Copy (conf.path, Filename.concat bundle_dir exe_file);
+let create_bundle cli =
+  let create_bundle global_options conf () =
+    OpamArg.apply_global_options cli global_options;
+    OpamGlobalState.with_ `Lock_read @@ fun gt ->
+    OpamSwitchState.with_ `Lock_read gt @@ fun st ->
+    let package =
+    OpamSwitchState.find_installed_package_by_name st conf.package
+    in
+    let opam = OpamSwitchState.opam st package in
+    let bin_path =
+      OpamFilename.Dir.to_string
+        (OpamPath.Switch.bin gt.root st.switch st.switch_config)
+    in
+    let binaries =
+      OpamPath.Switch.changes gt.root st.switch conf.package
+      |> OpamFile.Changes.safe_read
+      |> OpamStd.String.Map.keys
+      |> List.filter_map (fun name ->
+          let bin = OpamStd.String.remove_prefix ~prefix:"bin/" name in
+          if String.equal bin name then None
+          else Some bin)
+    in
+    let binary_path =
+      match conf.path, conf.binary with
+      | Some _, Some _ ->
+        OpamConsole.error_and_exit `Bad_arguments
+          "--binary-path and --binary can't be used together"
+      | Some path, None ->
+        if Sys.file_exists path then
+          if OpamSystem.is_exec path then
+            OpamConsole.error_and_exit `Bad_arguments
+              "File %s is not executable" path
+          else path
+        else
+          OpamConsole.error_and_exit `Not_found
+            "File not found at %s" path
+      | None, Some binary ->
+        if List.exists (String.equal binary) binaries then
+          Filename.concat bin_path binary
+        else
+          OpamConsole.error_and_exit `Not_found
+            "Binary %s not found in opam installation" binary
+      | None, None ->
+        match binaries with
+        | [bin] ->
+          Filename.concat bin_path bin
+        | [] ->
+          OpamConsole.error_and_exit `Not_found
+            "No binary file found at package installation %s"
+            (OpamPackage.to_string package)
+        | _::_ ->
+          OpamConsole.error_and_exit `False
+            "owix don't handle yet several binaries, \
+             choose one in the list and give it in argument \
+             with option '--binary':%s"
+            (OpamStd.Format.itemize (fun x -> x) binaries)
+    in
+    let dlls =Cygcheck.get_dlls binary_path in
+    let bundle_dir =
+      Filename.concat conf.output_dir
+        (OpamPackage.to_string package)
+    in
+    if Sys.file_exists bundle_dir then begin
+      System.call_unit System.Remove (true, bundle_dir)
+    end;
+    System.call_unit System.Mkdir (true, bundle_dir);
+    List.iter (fun dll ->
+        System.call_unit System.Copy (dll, bundle_dir)
+      ) dlls;
+    let exe_file =
+      let base = Filename.basename binary_path in
+      if not (Filename.extension base = "exe")
+      then base ^ ".exe"
+      else base
+    in
+    System.call_unit System.Copy (binary_path, Filename.concat bundle_dir exe_file);
     System.call_unit System.Copy (conf.icon_file, bundle_dir);
     System.call_unit System.Copy (conf.dlg_bmp, bundle_dir);
     System.call_unit System.Copy (conf.ban_bmp, bundle_dir);
     let module Info = struct
+      open OpamStd.Option.Op
       let path = bundle_dir
-      let package_name = "frama-c"
-      let package_version = "2.8.1"
-      let description = "Pipiska"
-      let manufacter = "OcamlPro"
+      let package_name =
+        OpamPackage.Name.to_string (OpamPackage.name package)
+      let package_version =
+        OpamPackage.Version.to_string (OpamPackage.version package)
+      let description =
+        (OpamFile.OPAM.synopsis opam)
+        ++ (OpamFile.OPAM.descr_body opam)
+        +! (Printf.sprintf "Package %s - binary %s"
+              (OpamPackage.to_string package)
+              (Filename.basename binary_path))
+      let manufacter =
+        String.concat ", "
+          (OpamFile.OPAM.maintainer opam)
       let package_guid = conf.package_guid
-      let tags = ["Huy"; "Prosto"; "Ser"]
+      let tags = OpamFile.OPAM.tags opam
       let exec_file = exe_file
       let dlls = List.map Filename.basename dlls
       let icon_file = Filename.basename conf.icon_file
@@ -94,24 +174,46 @@ let create_bundle conf =
     Wix.write_wxs "frama-c.wxs" wxs;
     System.call_unit System.Candle (conf.wix_path, ["frama-c.wxs"; "data/wix/CustomInstallDir.wxs"; "data/wix/CustomInstallDirDlg.wxs"]);
     System.call_unit System.Light (conf.wix_path, ["frama-c.wixobj"; "CustomInstallDir.wixobj"; "CustomInstallDirDlg.wixobj"],
-      ["WixUIExtension"; "WixUtilExtension"], Filename.concat conf.output_dir "frama-c.msi")
-(*     System.call_unit System.Remove (false, "frama-c-gui.wxs");
-    System.call_unit System.Remove (false, "frama-c-gui.wixobj");
-    System.call_unit System.Remove (false, "CustomInstallDir.wixobj");
-    System.call_unit System.Remove (false, "CustomInstallDirDlg.wixobj")
- *)
-(*     System.call_unit System.Remove (true, bundle_dir);
- *)
+                                   ["WixUIExtension"; "WixUtilExtension"], Filename.concat conf.output_dir "frama-c.msi");
+    (*     System.call_unit System.Remove (false, "frama-c-gui.wxs");
+           System.call_unit System.Remove (false, "frama-c-gui.wixobj");
+           System.call_unit System.Remove (false, "CustomInstallDir.wixobj");
+           System.call_unit System.Remove (false, "CustomInstallDirDlg.wixobj")
+    *)
+    (*     System.call_unit System.Remove (true, bundle_dir);
+    *)
+    OpamSwitchState.drop st;
+    OpamGlobalState.drop gt
+  in
+  let doc =
+    "Windows msi bundeling for cygwin program"
+  in
+  let man = [
+    `S Manpage.s_description;
+    `P "owix..........";
+  ]
+  in
+  OpamArg.mk_command ~cli OpamArg.cli_original "owix" ~doc ~man
+    Term.(const create_bundle
+          $ OpamArg.global_options cli
+          $ Args.term)
+
+let () =
+  OpamSystem.init ();
+  (* OpamArg.preinit_opam_envvariables (); *)
+  OpamCliMain.main_catch_all @@ fun () ->
+(*
+  for cmdliner 1.2.0
+  let term, info = create_bundle (OpamCLIVersion.default, `Default) in
+  exit @@ Cmd.eval ~catch:false (Cmd.v info term)
+*)
+  let terminfo = create_bundle (OpamCLIVersion.default, `Default) in
+  match Term.eval ~catch:false terminfo with
+  | `Error _ -> exit 1
+  | _ -> exit 0
 
 
-let main_info =
-  Cmd.info
-    ~doc:"Windows msi bundeling for cygwin program"
-    "owix"
-
-let main_term = Term.(const create_bundle $ Args.term)
-
-
+(*
 let () =
   match
     Cmd.eval_value ~catch:false (Cmd.v main_info main_term)
@@ -125,3 +227,4 @@ let () =
     exit 2
   | Error _ -> exit 3
   | _ -> exit 0
+*)
