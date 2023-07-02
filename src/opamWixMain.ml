350421333de6e23d7474b884f44765b3bc979256
Mohamed Hernouf <mohamed.hernouf@ocamlpro.com>
2023-07-02 17:13:13 +0200

    Man page

--- src/opamWixMain.ml
+++ src/opamWixMain.ml
@@ -130,7 +130,7 @@ let create_bundle cli =
             (OpamPackage.to_string package)
         | _::_ ->
           OpamConsole.error_and_exit `False
-            "owix don't handle yet several binaries, \
+            "opam-wix don't handle yet several binaries, \
              choose one in the list and give it in argument \
              with option '--binary'."
     in
@@ -206,14 +206,31 @@ let create_bundle cli =
     OpamGlobalState.drop gt
   in
   let doc =
-    "Windows msi bundeling for cygwin program"
+    "Windows MSI installer generation for Opam packages"
   in
   let man = [
     `S Manpage.s_description;
-    `P "owix..........";
+    `P "This utility is an opam plugin that generates a standalone MSI file. This file is \
+    used by Windows Installer to make available a chosen executable from an Opam package \
+    throughout the entire system.";
+    `P "Generated MSI indicates to system to create an installation directory named \
+    '$(b,Package Version.Executable)' under \"Program Files\" folder and to store there \
+    the following items:";
+    `I ("$(i,executable.exe)",
+      "The selected executable file from package 'PACK'. There are two options how to indicate \
+      where to find this binary. First, is to let opam find binary with the same name for you with \
+      $(b,-b) option. Second, is to use the path to binary with $(b,--bp) option. In this case binary \
+      will be considered as a part of package and its metadata.");
+    `I ("$(i,*.dll)",
+      "All executable's dependencies libriries found with $(b,cygcheck).");
+    `I ("$(i,icon and *.bmp)",
+      "Additional files used by installer to customise GUI. Options ($b,--ico), ($b,--dlg-bmp) and ($b,--ban-bmp) \
+      could be used to bundle custom files.");
+    `P "Additionnaly, installer gives to user a possibility to create a shortcut on Desktop and Start menu as well as \
+    adding installation folder to the PATH."
   ]
   in
-  OpamArg.mk_command ~cli OpamArg.cli_original "owix" ~doc ~man
+  OpamArg.mk_command ~cli OpamArg.cli_original "opam-wix" ~doc ~man
     Term.(const create_bundle
           $ OpamArg.global_options cli
           $ Args.term)
